66000186431fdd1928aa2b897ef396ceb7719ce2
kimi5231 <djwjod@naver.com>
2023-10-12 21:02:09 +0900

    added need file

---
+++ boy.py
@@ -0,0 +1,152 @@
+from pico2d import load_image, get_time
+import math
+from sdl2 import SDL_KEYDOWN, SDLK_SPACE, SDLK_RIGHT, SDL_KEYUP, SDLK_LEFT
+
+
+def space_down(e):
+    return e[0] == 'INPUT' and e[1].type == SDL_KEYDOWN and e[1].key == SDLK_SPACE
+
+
+def time_out(e):
+    return e[0] == 'TIME_OUT'
+
+
+def right_down(e):
+    return e[0] == 'INPUT' and e[1].type == SDL_KEYDOWN and e[1].key == SDLK_RIGHT
+
+
+def right_up(e):
+    return e[0] == 'INPUT' and e[1].type == SDL_KEYUP and e[1].key == SDLK_RIGHT
+
+
+def left_down(e):
+    return e[0] == 'INPUT' and e[1].type == SDL_KEYDOWN and e[1].key == SDLK_LEFT
+
+
+def left_up(e):
+    return e[0] == 'INPUT' and e[1].type == SDL_KEYUP and e[1].key == SDLK_LEFT
+
+
+class RUN:
+    @staticmethod
+    def enter(boy, e):
+        if right_down(e) or left_up(e):
+            boy.dir, boy.action = 1, 1
+        if left_down(e) or right_up(e):
+            boy.dir, boy.action = -1, 0
+
+
+    @staticmethod
+    def exit(boy, e):
+        pass
+
+    @staticmethod
+    def do(boy):
+        boy.frame = (boy.frame + 1) % 8
+        boy.x += boy.dir * 5
+
+    @staticmethod
+    def draw(boy):
+        boy.image.clip_draw(boy.frame * 100, boy.action * 100, 100, 100, boy.x, boy.y)
+
+class Sleep:
+    @staticmethod
+    def enter(boy, e):
+        boy.frame = 0
+        pass
+
+    @staticmethod
+    def exit(boy, e):
+        pass
+
+    @staticmethod
+    def do(boy):
+        boy.frame = (boy.frame + 1) % 8
+
+    @staticmethod
+    def draw(boy):
+        if boy.action == 2:
+            boy.image.clip_composite_draw(boy.frame * 100, boy.action * 100, 100, 100,
+                                      -math.pi/2, ' ', boy.x - 25, boy.y - 25, 100, 100)
+        else:
+            boy.image.clip_composite_draw(boy.frame * 100, boy.action * 100, 100, 100,
+                                          math.pi / 2, ' ', boy.x - 25, boy.y - 25, 100, 100)
+class Idle:
+    @staticmethod
+    def enter(boy, e):
+        if boy.action == 0:
+            boy.action = 2
+        elif boy.action == 1:
+            boy.action = 3
+        boy.dir = 0
+        boy.frame = 0
+        boy.start_time = get_time()
+        pass
+
+    @staticmethod
+    def exit(boy, e):
+        pass
+
+    @staticmethod
+    def do(boy):
+        boy.frame = (boy.frame + 1) % 8
+        if get_time() - boy.start_time > 2:
+            boy.state_machine.handle_event(('TIME_OUT', 0))
+
+    @staticmethod
+    def draw(boy):
+        boy.image.clip_draw(boy.frame * 100, boy.action * 100, 100, 100, boy.x, boy.y)
+
+
+class StateMachine:
+    def __init__(self, boy):
+        self.boy = boy
+        self.cur_state = Idle
+        self.table = {
+            Idle: {right_down: RUN, right_up: RUN, left_down: RUN, left_up: RUN, time_out: Sleep},
+            RUN: {right_down: Idle, right_up: Idle, left_down: Idle, left_up: Idle},
+            Sleep: {right_down: RUN, right_up: RUN, left_down: RUN, left_up: RUN, space_down: Idle}
+        }
+        pass
+    def start(self):
+        self.cur_state.enter(self.boy, ('START', 0))
+        pass
+    def update(self):
+        self.cur_state.do(self.boy)
+        pass
+
+    def handle_event(self, e):
+        for check_event, next_state in self.table[self.cur_state].items():
+            if check_event(e):
+                self.cur_state.exit(self.boy, e)
+                self.cur_state = next_state
+                self.cur_state.enter(self.boy, e)
+                return True #성공적으로 이벤트 변환
+        return False #이벤트를 소모하지 못함
+        pass
+
+    def draw(self):
+        self.cur_state.draw(self.boy)
+        pass
+    pass
+
+
+class Boy:
+    def __init__(self):
+        self.x, self.y = 400, 90
+        self.frame = 0
+        self.dir = 0
+        self.action = 3
+        self.image = load_image('animation_sheet.png')
+        self.state_machine = StateMachine(self)
+        self.state_machine.start()
+
+    def update(self):
+        self.state_machine.update()
+
+    def handle_event(self, event):
+        self.state_machine.handle_event(('INPUT', event))
+        pass
+
+    def draw(self):
+        self.state_machine.draw()
